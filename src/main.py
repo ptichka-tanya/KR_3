89b635783be68ed1993021d1c958a2a5675d6f87
ptichka-tanya <t.n.sinitsina@gmail.com>
2023-12-15 20:35:13 +0300

    Доработка функции main и тестирование функций

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from datetime import datetime
 
-from config import TRANSACTIONS_PATH
-from functions import mask_card_acc_number, format_date, load_data
+from src.config import TRANSACTIONS_PATH
+from src.functions import mask_card_acc_number, format_date, load_data
 
 
-def main():
-    data: list[dict] = load_data(TRANSACTIONS_PATH)
+def get_5_last_transactions(path: str):
+    data: list[dict] = load_data(path)
 
     executed_transactions: list = [transaction for transaction in data if transaction.get("state") == "EXECUTED"]
 
     last_5_transactions: list = sorted(executed_transactions,
                                        key=lambda x: datetime.strptime(x["date"], '%Y-%m-%dT%H:%M:%S.%f'),
                                        reverse=True)[:5]
+
+    transactions_output = []
 
     for transaction in last_5_transactions:
         date = format_date(transaction['date'])
@@ -23,8 +25,15 @@ def main():
             masked_from = mask_card_acc_number(transaction['from'])
         else:
             masked_from = ''
-        print(f"{date} {description}\n{masked_from} -> {masked_to}\n{amount} {currency}\n")
+        transactions_output.append(f"{date} {description}\n{masked_from} -> {masked_to}\n{amount} {currency}\n")
+
+    return transactions_output
+
+
+def main():
+    for transaction in get_5_last_transactions(TRANSACTIONS_PATH):
+        print(transaction)
 
 
 if __name__ == '__main__':
-    main()
+    main() # pragma: no cover
